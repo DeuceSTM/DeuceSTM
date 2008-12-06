8a603d17e7eb7917fd2ab93986c3618a00171d4f
Guy Korland <gkorland@gmail.com>
2008-12-06 13:22:54 +0000

    Add fields tests

--- src/test/org/deuce/test/parallel/StaticUpdate.java
+++ src/test/org/deuce/test/parallel/StaticUpdate.java
@@ -11,35 +11,35 @@ import org.junit.Test;
 public class StaticUpdate {
 
 	private static int intVar;
-//	private static long longVar;
-//	private static double doubleVar;
-//	private static Object objectVar;
-//	private static String stringVar;
+	private static long longVar;
+	private static double doubleVar;
+	private static Object objectVar;
+	private static String stringVar;
 	
-//	private static int[] intArrVar;
-//	private static long[] longArrVar;
-//	private static double[] doubleArrVar;
-//	private static Object[] objectArrvar;
-//	private static String[] stringArrvar;
+	private static int[] intArrVar;
+	private static long[] longArrVar;
+	private static double[] doubleArrVar;
+	private static Object[] objectArrvar;
+	private static String[] stringArrvar;
 
 	@Before 
     public void setUp() { 
     	intVar = 0;
-//    	longVar = 0;
-//    	doubleVar = 0;
-//    	objectVar = null;
-//    	stringVar = null;
+    	longVar = 0;
+    	doubleVar = 0;
+    	objectVar = null;
+    	stringVar = null;
     	
-//    	intArrVar = new int[1];
-//    	longArrVar = new long[1];
-//    	doubleArrVar = new double[1];
-//    	objectArrvar = new Object[1];
-//    	stringArrvar = new String[1];
+    	intArrVar = new int[1];
+    	longArrVar = new long[1];
+    	doubleArrVar = new double[1];
+    	objectArrvar = new Object[1];
+    	stringArrvar = new String[1];
 	}
 	
 	@Test
 	public void simpleAdd() throws InterruptedException {
-		int threadsCount = 2;
+		int threadsCount = 4;
 		final int rounds = 10000;
 		Thread[] threads = new Thread[threadsCount];
 		
@@ -73,32 +73,32 @@ public class StaticUpdate {
 		
 		
 		Assert.assertEquals( rounds*threadsCount, intVar);
-//		Assert.assertEquals( rounds*threadsCount, longVar);
-//		Assert.assertEquals( (double)rounds*threadsCount, doubleVar);
-////		Assert.assertNotNull(objectVar);
-////		Assert.assertEquals( "a", stringVar);
-//		
-//		Assert.assertEquals( rounds*threadsCount, intArrVar[0]);
-//		Assert.assertEquals( rounds*threadsCount, longArrVar[0]);
-//		Assert.assertEquals( (double)rounds*threadsCount, doubleArrVar[0]);
-//		Assert.assertNotNull(objectArrvar[0]);
-//		Assert.assertEquals( "a", stringArrvar[0]);
+		Assert.assertEquals( rounds*threadsCount, longVar);
+		Assert.assertEquals( (double)rounds*threadsCount, doubleVar);
+		Assert.assertNotNull(objectVar);
+		Assert.assertEquals( "a", stringVar);
+		
+		Assert.assertEquals( rounds*threadsCount, intArrVar[0]);
+		Assert.assertEquals( rounds*threadsCount, longArrVar[0]);
+		Assert.assertEquals( (double)rounds*threadsCount, doubleArrVar[0]);
+		Assert.assertNotNull(objectArrvar[0]);
+		Assert.assertEquals( "a", stringArrvar[0]);
 		
 	}
 	
 	@Atomic
 	private void atomicSingleUpdate() {
 		intVar++;
-//		longVar++;
-//		doubleVar++;
-//		objectVar = new Object();
-//		stringVar = new String("a");
+		longVar++;
+		doubleVar++;
+		objectVar = new Object();
+		stringVar = new String("a");
 		
-//		intArrVar[0]++;
-//    	longArrVar[0]++;
-//    	doubleArrVar[0]++;
-//    	objectArrvar[0] = new Object();
-//    	stringArrvar[0] = new String("a");
+		intArrVar[0]++;
+    	longArrVar[0]++;
+    	doubleArrVar[0]++;
+    	objectArrvar[0] = new Object();
+    	stringArrvar[0] = new String("a");
 	}
 	
 //	@Test 
