4d842f6bbc1d85ff8ac3a754c79f7da66da7f0dc
Guy Korland <gkorland@gmail.com>
2009-03-11 21:18:31 +0000

    Issue 34 Add a test for the bug.

---
+++ src/test/org/deuce/test/reflection/AnnotationTest.java
@@ -0,0 +1,33 @@
+package org.deuce.test.reflection;
+
+import java.lang.annotation.Annotation;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.reflect.Method;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AnnotationTest {
+
+	@Test
+	public void annotation() throws Exception {
+		MyClass c = new MyClass();
+		Method declaredMethod = c.getClass().getDeclaredMethod("myMethod");
+		Assert.assertNotNull(declaredMethod.getAnnotation(MyAnnotation.class));
+	}
+
+
+	@Retention(RetentionPolicy.RUNTIME)
+	public @interface MyAnnotation {
+		String value() default "";
+	}
+
+	public static class MyClass {
+		@MyAnnotation
+		public void myMethod() {}
+	}
+
+
+
+}
