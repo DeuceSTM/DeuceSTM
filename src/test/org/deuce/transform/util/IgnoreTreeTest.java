6a8eac5239783f8ff141d57f447567a147d72494
Guy Korland <gkorland@gmail.com>
2009-06-12 21:39:31 +0000

    Change test from JUnit 4 to JUnit 3.

--- src/test/org/deuce/transform/util/IgnoreTreeTest.java
+++ src/test/org/deuce/transform/util/IgnoreTreeTest.java
@@ -1,85 +1,72 @@
 package org.deuce.transform.util;
 
 import junit.framework.Assert;
+import junit.framework.TestCase;
 
-import org.junit.Test;
 
-
-public class IgnoreTreeTest {
+public class IgnoreTreeTest extends TestCase {
 	
 	
-	@Test
-	public void checkContainsAll(){
+	public void testCheckContainsAll(){
 		IgnoreTree tree = new IgnoreTree("*");
 		Assert.assertTrue( tree.contains("java/lang/String"));
 	}
 	
-	@Test
-	public void checkContainsAllJava(){
+	public void testCheckContainsAllJava(){
 		IgnoreTree tree = new IgnoreTree("java");
 		Assert.assertTrue( tree.contains("java/lang/String"));
 	}
 	
-	@Test
-	public void checkContainsAllJavaStar(){
+	public void testCheckContainsAllJavaStar(){
 		IgnoreTree tree = new IgnoreTree("java.*");
 		Assert.assertTrue( tree.contains("java/lang/String"));
 	}
 	
 	
-	@Test
-	public void checkContains(){
+	public void testCheckContains(){
 		IgnoreTree tree = new IgnoreTree("java.lang");
 		Assert.assertTrue( tree.contains("java/lang/String"));
 	}
 	
-	@Test
-	public void checkContainsClass(){
+	public void testCheckContainsClass(){
 		IgnoreTree tree = new IgnoreTree("java.lang.String");
 		Assert.assertTrue( tree.contains("java/lang/String"));
 	}
 	
-	@Test
-	public void checkContainsFewClass(){
+	public void testCheckContainsFewClass(){
 		IgnoreTree tree = new IgnoreTree("java.lang.*");
 		Assert.assertTrue( tree.contains("java/lang/String"));
 		Assert.assertTrue( tree.contains("java/lang/Integer"));
 	}
 	
-	@Test
-	public void checkContainsFewPackages(){
+	public void testCheckContainsFewPackages(){
 		IgnoreTree tree = new IgnoreTree("java.lang.*,java.util.*");
 		Assert.assertTrue( tree.contains("java/lang/String"));
 		Assert.assertTrue( tree.contains("java/util/HashMap"));
 	}
 	
-	@Test
-	public void checkContainsFewPackagesAndClasses(){
+	public void testCheckContainsFewPackagesAndClasses(){
 		IgnoreTree tree = new IgnoreTree("java.util.*,java.lang.String");
 		Assert.assertTrue( tree.contains("java/lang/String"));
 		Assert.assertTrue( tree.contains("java/util/HashMap"));
 	}
 	
-	@Test
-	public void checkContainsFewPackagesAndContainsClasses(){
+	public void testCheckContainsFewPackagesAndContainsClasses(){
 		IgnoreTree tree = new IgnoreTree("java.lang.*,java.lang.String");
 		Assert.assertTrue( tree.contains("java/lang/String"));
 	}
 	
-	@Test
-	public void checkNotContains(){
+	public void testCheckNotContains(){
 		IgnoreTree tree = new IgnoreTree("java.lang");
 		Assert.assertFalse( tree.contains("java/util/HashMap"));
 	}
 	
-	@Test
-	public void checkNotContainsAll(){
+	public void testCheckNotContainsAll(){
 		IgnoreTree tree = new IgnoreTree("java.*");
 		Assert.assertFalse( tree.contains("com/sun/HashMap"));
 	}
 	
-	@Test
-	public void checkNotContainsClass(){
+	public void testCheckNotContainsClass(){
 		IgnoreTree tree = new IgnoreTree("java.lang.String");
 		Assert.assertFalse( tree.contains("java/lang/Integer"));
 	}
